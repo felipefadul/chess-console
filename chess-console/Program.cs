cae92ce8e6aeeb44fc918ce80608584d9ce1207f
Felipe Fadul <felipe.fadul@hotmail.com>
2022-09-20 18:10:25 -0300

    feat: Add ChessPosition class

--- chess-console/Program.cs
+++ chess-console/Program.cs
@@ -8,20 +8,9 @@ namespace chess_console
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Board board = new(8, 8);
-
-                board.PlaceAPiece(new Rook(board, Color.Black), new Position(0, 0));
-                board.PlaceAPiece(new Rook(board, Color.Black), new Position(1, 3));
-                board.PlaceAPiece(new King(board, Color.Black), new Position(0, 2));
-
-                Screen.PrintBoard(board);
-            }
-            catch (BoardException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            ChessPosition chessPosition = new('c', 7);
+            Console.WriteLine(chessPosition);
+            Console.WriteLine(chessPosition.ToPosition());
 
             Console.ReadLine();
         }
---
+++ chess-console/board/chess/ChessPosition.cs
@@ -0,0 +1,26 @@
+﻿namespace board.chess
+{
+    internal class ChessPosition
+    {
+
+        public char Column { get; set; }
+        public int Row { get; set; }
+        private const int MAXIMUM_NUMBER_OF_ROWS_POSITIONS = 8;
+
+        public ChessPosition(char column, int row)
+        {
+            Column = column;
+            Row = row;
+        }
+
+        public override string ToString()
+        {
+            return "" + Column.ToString().ToUpper() + Row;
+        }
+
+        public Position ToPosition()
+        {
+            return new Position(MAXIMUM_NUMBER_OF_ROWS_POSITIONS - Row, Column - 'a');
+        }
+    }
+}
